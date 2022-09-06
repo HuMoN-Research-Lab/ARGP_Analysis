16d4b148421448d258534a278a4aef7a0ae8a7b6
Trent Wirth <trentondwirth@gmail.com>
2022-09-06 12:47:38 -0400

    get "frame_marker_dimension" dataframe
    took `.mat` data from qualisys, reshaped and saved it, and then made it a dataframe in python.

--- qualisys_to_python/load_data_from_qualisys.m
+++ qualisys_to_python/load_data_from_qualisys.m
@@ -1,33 +1,31 @@
+qualisys_file_name = '2022-08-29_Pilot_Data0002.mat'; %Qualisys mat file name 
 
-sessionID = 'qualisys_sesh_2022-05-24_15_10_49_JSM_T2_slackline'; %sessionID name
-qualisys_file_name = 'argp_pilot_2022_04_29_TDW.mat'; %Qualisys mat file name 
+%% 
+filepath = fullfile([pwd,filesep,qualisys_file_name]);
 
-%change anything before 'FreeMocap_Data' to match your path 
-filepath = fullfile('C:\Users\miken\OneDrive\Documents\GitHub\ARGP_Python_Pipeline\Data\' ,qualisys_file_name);
+data = load(filepath);
 
-mat_data = load(filepath);
+qtm_struct_name = fieldnames(data);
 
-
-labeled_marker_data = mat_data.argp_pilot_2022_04_29_TDW.Trajectories.Labeled.Data; % Change the 'argp_pilot...' part of this path to match what's in your path
+labeled_marker_data = data.(qtm_struct_name{1}).Trajectories.Labeled.Data; 
 
 shape_marker_data = size(labeled_marker_data);
+
+%%
 
 num_markers = shape_marker_data(1);
 num_coordinates = 3;
 num_frames = shape_marker_data(3);
 
-test_data = labeled_marker_data(:,1:3,1:5);
-
-mat_data_reshaped = nan(num_frames, num_markers, 3);
-
+data_reshaped = nan(num_frames, num_markers, 3);
 
 for frame = 1:num_frames
     for marker = 1:num_markers
         for coordinate = 1:num_coordinates
-    mat_data_reshaped(frame,marker,coordinate) = labeled_marker_data(marker,coordinate,frame);
+    data_reshaped(frame,marker,coordinate) = labeled_marker_data(marker,coordinate,frame);
         end
     end
 end
 
-savepath = fullfile('C:\Users\miken\OneDrive\Documents\GitHub\ARGP_Python_Pipeline\Data\qualisys_markers_3d');
-save(savepath,'mat_data_reshaped')
+savepath = fullfile([pwd,filesep,'qualisys_data_reshaped']);
+save(savepath,'data_reshaped')
