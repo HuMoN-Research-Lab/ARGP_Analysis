659b9a57a99753ccaf31e813702b1ef3eb0a3f23
Trent Wirth <trentondwirth@gmail.com>
2022-09-26 16:17:50 -0400

    Update proccess_data.py

--- proccess_data.py
+++ proccess_data.py
@@ -7,7 +7,7 @@ from utilities.qualisys_json_creator import qualisys_json_creator
 from utilities.qualisys_to_generic_skeleton_converter import qualisys_to_generic_skeleton_converter
 
 
-def process_data(subject_json_path: Path, 
+def process_data(subject_json_path: Path,
                  qualisys_df: pandas.core.frame.DataFrame,
                  pupil_df: pandas.core.frame.DataFrame) -> np.array:
 
