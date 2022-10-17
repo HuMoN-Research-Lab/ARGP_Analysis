d5e4ba69d77b5a4a1c1ecabb832232b85c3a9cc6
Trent Wirth <trentondwirth@gmail.com>
2022-10-17 18:25:20 -0400

    note for tomorrow

--- pupil_labs_stuff/convert_pupil_labs_data_class_to_dict.py
+++ pupil_labs_stuff/convert_pupil_labs_data_class_to_dict.py
@@ -4,4 +4,7 @@ from pupil_labs_stuff.data_classes.pupil_dataclass_and_handler import PupilLabsD
 def convert_pupil_labs_data_class_to_dict(left_eye_pupil_labs_data: PupilLabsDataClass,
                                           right_eye_pupil_labs_data: PupilLabsDataClass) -> dict:
 
+    # TODO go through pupil labs data classes and build a pupil_dict
+    # the pupil dictionary will be used in the synchronization process
+
     pass
--- pupil_labs_stuff/pupil_qualisys_synchronizer.py
+++ pupil_labs_stuff/pupil_qualisys_synchronizer.py
@@ -4,4 +4,3 @@ class PupilFreemocapSynchronizer:
 
     def __init__(self, generic_skelly_dict: dict):
         self.generic_skelly_dict = generic_skelly_dict
-        
