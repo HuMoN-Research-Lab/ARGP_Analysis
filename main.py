8a76d823c7e769671e20654518fdc2e1a204797b
Trent Wirth <trentondwirth@gmail.com>
2022-10-19 10:11:17 -0400

    created timestamp file

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import pandas as pd
 from utilities.create_generic_skeleton_from_qualisys_data import create_generic_skeleton_from_qualisys_data
 from utilities.create_laser_skeleton import create_laser_skeleton
 from utilities.data_vis_debug import data_vis_debug
+from utilities.get_qualisys_unix_timestamps import get_qualisys_unix_timestamps
 
 logger = logging.getLogger(__name__)
 # logger.setLevel(INFO)
 
 if __name__ == "__main__":
-
     # move lines 12-20 to a function or separate functions
     base_data_path = Path('D:\\data_storage\\argp_data')
     subject_id = '2022_08_29_Pilot_Data0002'
@@ -25,7 +25,6 @@ if __name__ == "__main__":
     qualisys_file_name = '2022-08-29_Pilot_Data0002.tsv'
     pupil_file_name = 'pupil_positions.csv'
 
-
     # VOR Frames
     vor_start = 2500
     vor_end = 6000
@@ -37,18 +36,7 @@ if __name__ == "__main__":
     qualisys_df = pd.read_csv(filepath_or_buffer=str(qualisys_data_path), delimiter='\t', header=11)
     pupil_df = pd.read_csv(filepath_or_buffer=str(pupil_data_path), delimiter=',', header=0)
 
-    qualisys_start_time_string = pd.read_csv(filepath_or_buffer=str(qualisys_data_path), delimiter='\t', skiprows=7, nrows=1, header=None)[1][0]
-
-    qualisys_start_time_unix_local_time = pd.Timestamp(qualisys_start_time_string.replace(",", "")).timestamp()
-
-    time_zone_offset_in_seconds = 4*60*60  # TODO pull time manipulation into another function
-
-    pupil_qualisys_temporal_offset_in_seconds = 0  # TODO Figure out the hard coded offset for this dataset.
-
-    qualisys_start_time_unix = qualisys_start_time_unix_local_time + time_zone_offset_in_seconds + pupil_qualisys_temporal_offset_in_seconds
-
-    # TODO add the unix_npy to the qualisys_df
-    qualisys_timestamps_unix_npy = qualisys_df['Time'].to_numpy() + qualisys_start_time_unix
+    qualisys_timestamps_unix_npy = get_qualisys_unix_timestamps(qualisys_df, qualisys_data_path)
 
     subject_json_path = qualisys_data_path / base_data_path / subject_id / 'processing_jsons'
 
@@ -57,8 +45,6 @@ if __name__ == "__main__":
     subject_qualisys_json_path = subject_json_path / 'qualisys_dict.json'
 
     generic_skelly_dict = create_generic_skeleton_from_qualisys_data(subject_qualisys_json_path, qualisys_df)
-
-
 
     create_laser_skeleton(generic_skelly_dict=generic_skelly_dict,
                           pupil_df=pupil_df,
@@ -70,6 +56,3 @@ if __name__ == "__main__":
     data_vis_debug(generic_skelly_dict, select_frame=np.array([1500]))
 
     f = 'debug_stop'
-
-
-
---
+++ utilities/get_qualisys_unix_timestamps.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+
+def get_qualisys_unix_timestamps(qualisys_df: pd.DataFrame,
+                                 qualisys_data_path: Path) -> np.ndarray:
+
+    qualisys_start_time_string = pd.read_csv(filepath_or_buffer=str(qualisys_data_path), delimiter='\t', skiprows=7, nrows=1, header=None)[1][0]
+
+    qualisys_start_time_unix_local_time = pd.Timestamp(qualisys_start_time_string.replace(",", "")).timestamp()
+
+    time_zone_offset_in_seconds = 4*60*60
+
+    print('WARNING: pupil-qualisys temporal offset is hard-coded.')
+    pupil_qualisys_temporal_offset_in_seconds = 0  # TODO Figure out the hard coded offset for this dataset.
+
+    qualisys_start_time_unix = qualisys_start_time_unix_local_time + time_zone_offset_in_seconds + pupil_qualisys_temporal_offset_in_seconds
+
+    # TODO add the unix_npy to the qualisys_df
+    qualisys_timestamps_unix_npy = qualisys_df['Time'].to_numpy() + qualisys_start_time_unix
+
+    return qualisys_timestamps_unix_npy
